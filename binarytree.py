10d82d2e115704e494ef03c473ce5c8f02fee398
Roshan Wagle <roshanwagle@Roshans-MacBook-Pro.local>
2018-09-10 23:06:49 +1000

    Binary tree implementation

--- binarytree.py
+++ binarytree.py
@@ -1,10 +1,5 @@
-"""Creating a binary tree."""
-
-# first create a node class
-# second create a tree class
-
 class Node(object):
-    """Creating a Node."""
+    """Creating a node."""
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -12,36 +7,52 @@ class Node(object):
 
 
 class BinaryTree(object):
-    """Creating a tree."""
+    """Creating a binary tree"""
     def __init__(self, root):
-        self.root = Node(root)      # create a root node
+        self.root = Node(root)
 
-    def print_tree(self, traversal_type):
-        """Printing the traversal of a tree."""
-        if traversal_type == "preorder":
-            return self.preorder_print(tree.root, "")
+    def Inorder(self, start, traversal):
+        """Left-->Root-->Right"""
+        if start:
+            traversal = self.Inorder(start.left, traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.Inorder(start.right, traversal)
+        return traversal
 
-    def preorder_print(self, start, traversal):
-        """Root->Left->Right."""
+    def preorder(self, start, traversal):
+        """Root->Left->Right"""
         if start:
             traversal += (str(start.value) + "-")
-            traversal = self.preorder_print(start.left, traversal)
-            traversal = self.preorder_print(start.right, traversal)
+            traversal = self.preorder(start.left, traversal)
+            traversal = self.preorder(start.right, traversal)
         return traversal
-#           1
-#         /     \
-#        2      3
-#       /   \   /   \
-#       4   5   6   7
-# set up a tree
 
+    def postorder(self, start, traversal):
+        """Left-->Right-->Root"""
+        if start:
+            traversal = self.postorder(start.left, traversal)
+            traversal = self.postorder(start.right, traversal)
+            traversal += (str(start.value) + "-")
+        return traversal
 
-tree = BinaryTree(1)            # created an object of tree and a root node 1
-tree.root.left = Node(2)       # added a left child to the root Node
+    def printTree(self, traversal_type):
+        """Printing the tree."""
+        if traversal_type == 'preorder':
+            return self.preorder(tree.root, "")
+        elif traversal_type == 'Inorder':
+            return self.Inorder(tree.root, "")
+        else:
+            return self.postorder(tree.root, "")
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
-tree.root.right.right.right = Node(8)
-print(tree.print_tree("preorder"))
+#tree.root.right.right.right = Node(8)
+
+print(tree.printTree('preorder'))
+print(tree.printTree('Inorder'))
+print(tree.printTree('postorder'))
